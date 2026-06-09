72d5131f8e4e7a0683188242675bdc2efda29e29
FreakHuhn <mcbabamuck@googlemail.com>
2026-06-09 11:59:41 +0200

    added a Map function wich gets information via API, making a url get request

--- main.go
+++ main.go
@@ -8,24 +8,30 @@ import (
 )
 
 func main() {
-	scanner := bufio.NewScanner(os.Stdin)
-	for {
-		fmt.Print("Pokedex > ")
-		scanner.Scan()
-		if err := scanner.Err(); err != nil {
-			fmt.Fprintln(os.Stderr, "Error reading input:", err)
-			continue
-		}
-		input := cleanInput(strings.ToLower(scanner.Text()))
-		if len(input) == 0 {
-			continue
-		}
-		if command, ok := commandMap[input[0]]; ok {
-			if err := command.callback(); err != nil {
-				fmt.Fprintln(os.Stderr, "Error executing command:", err)
-			}
-		} else {
-			fmt.Fprintln(os.Stderr, "Unknown command:", input[0])
-		}
-	}
+    scanner := bufio.NewScanner(os.Stdin)
+
+    startURL := "https://pokeapi.co/api/v2/location-area"
+    cfg := &configStruct{
+        nextURL: &startURL,
+    }
+
+    for {
+        fmt.Print("Pokedex > ")
+        scanner.Scan()
+        if err := scanner.Err(); err != nil {
+            fmt.Fprintln(os.Stderr, "Error reading input:", err)
+            continue
+        }
+        input := cleanInput(strings.ToLower(scanner.Text()))
+        if len(input) == 0 {
+            continue
+        }
+        if command, ok := commandMap[input[0]]; ok {
+            if err := command.callback(cfg); err != nil {
+                fmt.Fprintln(os.Stderr, "Error executing command:", err)
+            }
+        } else {
+            fmt.Fprintln(os.Stderr, "Unknown command:", input[0])
+        }
+    }
 }
--- repl.go
+++ repl.go
@@ -1,7 +1,9 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
+	"net/http"
 	"os"
 	"strings"
 )
@@ -9,7 +11,22 @@ import (
 type cliCommand struct {
 	name string
 	description string
-	callback func() error
+	callback func(*configStruct) error
+}
+
+type configStruct struct {
+	nextURL *string 
+	previousURL *string
+}
+
+type requestStruct struct {
+	Count int `json:"count"`
+	Next *string `json:"next"`
+	Previous *string `json:"previous"`
+	Results []struct {
+		Name string `json:"name"`
+		URL string `json:"url"`
+	} `json:"results"`
 }
 
 var commandMap = map[string]cliCommand{
@@ -23,6 +40,11 @@ var commandMap = map[string]cliCommand{
 		description : "Displays a help message.",
 		callback : help,
 	},
+	"map": {
+		name: "map",
+		description: "Displays the next or previous page of Pokemon.",
+		callback: mapFunction,
+	},
 }
 
 var commandHelp = []struct {
@@ -31,9 +53,11 @@ var commandHelp = []struct {
 }{
     {"exit", "Exit the Pokedex"},
     {"help", "Displays a help message."},
+	{"map", "Displays the next or previous page of Pokemon."},
 }
 
-
+// Bereinigt die Benutzereingabe, indem sie in Kleinbuchstaben 
+// umgewandelt und in Wörter aufgeteilt wird
 func cleanInput(text string) []string {
 	list := strings.Split(text," ")
 	for i := 0; i < len(list); i++ {
@@ -45,16 +69,56 @@ func cleanInput(text string) []string {
 	return list
 }
 
-func commandExit() error {
+
+// Beendet die Anwendung
+func commandExit(cfg *configStruct) error {
 	fmt.Printf("Closing the Pokedex... Goodbye!\n")
 	os.Exit(0)
 	return nil
 }
 
-func help() error {
+// Gibt die liste der Befehle aus
+func help(cfg *configStruct) error {
 	fmt.Printf("Welcome to the Pokedex!\n")
 	for _, command := range commandHelp {
 		fmt.Printf("%s: %s\n", command.name, command.description)
+	}
+	return nil
+}
+
+
+// Macht Get request, unmarshalt die Antwort und gibt Struct zurück
+func makeRequest(url string) (requestStruct, error) {
+	var request requestStruct
+	response, err := http.Get(url)
+	if err != nil {
+		return request, err
+	}
+	defer response.Body.Close()
+
+	err = json.NewDecoder(response.Body).Decode(&request)
+	if err != nil {
+		return request, err
+	}
+	return request, nil
+}
+
+// Gibt die Namen der Zonen der nächsten Seite aus (mapb zurück).
+func mapFunction(cfg *configStruct) error {
+	var url *string
+	if cfg.nextURL != nil {
+		url = cfg.nextURL
+	} else {
+		url = cfg.previousURL
+	}
+	request, err := makeRequest(*url)
+	if err != nil {
+		return err
+	}
+	cfg.nextURL = request.Next
+	cfg.previousURL = request.Previous
+	for _, result := range request.Results {
+		fmt.Printf("%s\n", result.Name)
 	}
 	return nil
 }
