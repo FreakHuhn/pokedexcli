3aaa4c8c2baafbd85d6cbd58fa280baf24150dc5
FreakHuhn <mcbabamuck@googlemail.com>
2026-06-09 08:42:36 +0200

    cli pokedex incomming

--- main.go
+++ main.go
@@ -1,6 +1,22 @@
 package main
 
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strings"
+)
 
 func main() {
-	println("Hello, World!")
+	scanner := bufio.NewScanner(os.Stdin)
+	for {
+		fmt.Print("Pokedex > ")
+		scanner.Scan()
+		if err := scanner.Err(); err != nil {
+			fmt.Fprintln(os.Stderr, "Error reading input:", err)
+			continue
+		}
+		input := cleanInput(strings.ToLower(scanner.Text()))
+		fmt.Printf("Your command was: %s\n", input[0])
+	}
 }
